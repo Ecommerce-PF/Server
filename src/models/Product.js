2b9dc2de4448bcd30f6d333c61a8ce96759d5dd0
Eyver Vergara <EveloxD@gmail.com>
2023-06-07 01:28:49 -0500

    post products

---
+++ src/models/Product.js
@@ -0,0 +1,33 @@
+const DataTypes = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "Product",
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            price: {
+                type: DataTypes.FLOAT,
+                allowNull: false,
+            },
+            category: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            image: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const routesProducts = require("./routesProducts");
 const routesUsers = require("./routesUsers");
+const routePostProduct = require("./routePostProduct");
 
 
 
@@ -9,4 +10,5 @@ router.use("/products", routesProducts);
 
 router.use("/users", routesUsers);
 
+router.use("/postProduct", routePostProduct);
 module.exports = router;
